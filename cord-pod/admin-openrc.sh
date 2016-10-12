7fddfebdf9563a6e24b488ebbce4dc2ea585c999
Sean Stephens <seanastephens@email.arizona.edu>
2016-10-12 02:50:41 -0700

    First attempt at getting openvpn working

---
+++ cord-pod/admin-openrc.sh
@@ -0,0 +1,6 @@
+export OS_USERNAME=admin
+export OS_PASSWORD=VeryLongKeystoneAdminPassword
+export OS_TENANT_NAME=admin
+export OS_AUTH_URL=https://keystone.cord.lab:5000/v2.0
+export OS_REGION_NAME=RegionOne
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
